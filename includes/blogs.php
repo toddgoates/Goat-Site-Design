707206c9c7662558affdf03b1a28826b80709e11
Todd Goates <toddgoates@gmail.com>
2015-01-28 16:25:30 -0700

    Separate blog into it's own included file.  Put PHP include in thoughts.php

---
+++ includes/blogs.php
@@ -0,0 +1,14 @@
+<figure>
+    <a href="delete-all-the-things.php" class="thumb">
+        <div class="background">
+            <span class="caption">
+                <i class="fa fa-search"></i> Read More
+            </span>
+            <img src="img/thoughts/the_scream_thumb.jpg" alt="Delete All The Things">
+        </div>
+    </a>
+    <figcaption>
+        <strong>Delete All The Things!</strong>
+        <span class="posted">Nov 4th, 2014</span>
+    </figcaption>
+</figure>
--- thoughts.php
+++ thoughts.php
@@ -35,20 +35,9 @@
                 <section class="thoughts">
                     <h1>Some Thoughts</h1>
                     <p>Below are some different blog entries on the topic of technology and my quest to become the greatest web developer ever:</p>
-                    <figure>
-                        <a href="delete-all-the-things.php" class="thumb">
-                            <div class="background">
-                                <span class="caption">
-                                    <i class="fa fa-search"></i> Read More
-                                </span>
-                                <img src="img/thoughts/the_scream_thumb.jpg" alt="Delete All The Things">
-                            </div>
-                        </a>
-                        <figcaption>
-                            <strong>Delete All The Things!</strong>
-                            <span class="posted">Nov 4th, 2014</span>
-                        </figcaption>
-                    </figure>
+                    <?php
+                    include('includes/blogs.php');
+                    ?>
                 </section>
                 <section class="twitter">
                     <h2>Some Tweets</h2>
