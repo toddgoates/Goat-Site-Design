aa40b959f0abfdf163369c5c7065f8f5b9080bee
Todd Goates <toddgoates@gmail.com>
2015-02-09 15:39:46 -0700

    Improve spacing for menu icon.  Add margin to blog figures.  Add image for new blog.  Add new blog link to blogs.php.

--- includes/blogs.php
+++ includes/blogs.php
@@ -1,10 +1,27 @@
+<!-- Feb 9th, 2015 -->
+<figure>
+    <a href="sign-your-site.php" class="thumb">
+        <div class="background">
+            <span class="caption">
+                <i class="fa fa-search"></i> Read More
+            </span>
+            <img src="../img/thoughts/signature-thumb.jpg" alt="Sign Your Site">
+        </div>
+    </a>
+    <figcaption>
+        <strong>Sign Your Site</strong>
+        <span class="posted">Feb 9th, 2015</span>
+    </figcaption>
+</figure>
+
+<!-- Nov 4th, 2014 -->
 <figure>
     <a href="delete-all-the-things.php" class="thumb">
         <div class="background">
             <span class="caption">
                 <i class="fa fa-search"></i> Read More
             </span>
-            <img src="img/thoughts/the_scream_thumb.jpg" alt="Delete All The Things">
+            <img src="../img/thoughts/the_scream_thumb.jpg" alt="Delete All The Things">
         </div>
     </a>
     <figcaption>
