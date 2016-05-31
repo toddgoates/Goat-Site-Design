bb563703a584e11a2fdf09c0802720fdef49b92e
Todd Goates <toddgoates@gmail.com>
2016-05-30 20:31:50 -0600

    Restructured skills list to be database-driven to make code a little more DRY.

--- about.php
+++ about.php
@@ -1,3 +1,15 @@
+<?php
+  // Connect to the db
+  require_once('includes/db_vars.php');
+  $dbc = mysqli_connect($HOST, $USER, $PASS, $DB) or die('Could not connect to database');
+
+  // Build and run query
+  $query = "SELECT * FROM skills WHERE deleted = 0";
+  $result = mysqli_query($dbc, $query) or die('Query failed');
+
+  // Close db connection
+  mysqli_close($dbc);
+?>
 <!doctype html>
 <html lang="en">
 
@@ -7,17 +19,17 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<title>About Me | Todd Goates</title>
 	<meta name="description" content="Todd Goates is a Utah-based web designer and developer with a growing skill set and a love for awesome projects">
-    
-    <!-- Fonticons -->
-    <script src="https://use.fonticons.com/e9f434ee.js"></script>
+
+  <!-- Fonticons -->
+  <script src="https://use.fonticons.com/e9f434ee.js"></script>
 
 	<!-- CSS -->
 	<link href="css/main.min.css" rel="stylesheet">
-	
-    <?php
+
+  <?php
     include('includes/favicons.php');
-    ?>
-	
+  ?>
+
 	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
@@ -31,17 +43,17 @@
     include('includes/gtm.php');
     include('includes/header_nav.php');
 	?>
-	
+
 	<div class="main-content">
 		<div class="wrapper">
 			<section>
 				<h1>Hello World!</h1>
 				<img src="img/me.jpg" alt="Image of Todd Goates, a Utah-based Web Designer and Developer" class="me-pic">
-                <h2>Who am I?</h2>
+        <h2>Who am I?</h2>
 				<p>My name is Todd Goates. I was born in Connecticut and raised in Utah. I like to travel, spend time with my beautiful wife, listen to music/attempt to create it, draw, and code all sorts of cool projects. I am fresh out of college with a bachelor's degree in Digital Media with an emphasis in Internet Technologies from Utah Valley University. You might be thinking 'What the heck is a degree in Digital Media?' I define it as the love child of Computer Science and Graphic Design.</p>
 				<p>I have been drawing and designing for as long as I can remember. During my high school years, I discovered the how fun it was to code. I have loved programming for the web ever since I typed my first &lt;html&gt; tag years ago.</p>
-                <h2>What can I do for you?</h2>
-                <p>My goal is to smoothly solve your web dilemmas and to educate you on all the different technologies that can help your online presence.  Here are some of my services:</p>
+        <h2>What can I do for you?</h2>
+        <p>My goal is to smoothly solve your web dilemmas and to educate you on all the different technologies that can help your online presence.  Here are some of my services:</p>
 				<ul>
 					<li>Help build your brand</li>
 					<li>Design a slick website or web application</li>
@@ -50,8 +62,8 @@
 					<li>Site maintenance and repairs</li>
 					<li>Project Consultation</li>
 				</ul>
-                <h2>Why work with me?</h2>
-                <p>I am a passionate designer and developer who loves what he does.  I put my blood, sweat, and tears into every project I work on (not literally though).  You'll be proud of the final product, trust me.</p>
+        <h2>Why work with me?</h2>
+        <p>I am a passionate designer and developer who loves what he does.  I put my blood, sweat, and tears into every project I work on (not literally though).  You'll be proud of the final product, trust me.</p>
 				<p>Have something for me to work on?  Let's get started!</p>
 				<form action="contact.php" method="POST">
 					<button type="submit" class="cta-button">Start a Project</button>
@@ -59,250 +71,22 @@
 			</section>
 			<section id="skills">
 				<h2>Web Skills</h2>
-                <p><i class="fa fa-info-circle fa-fw"></i> Don't know what in the world I'm talking about?  Just flip the card over for the answer.</p>
-                <div class="skills-list">
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                HTML5
-                            </div>
-                            <div class="back">
-                                This is the semantic structure of your website.
-                            </div>
-                        </div>
+        <p><i class="fa fa-info-circle fa-fw"></i> Don't know what in the world I'm talking about?  Just flip the card over for the answer.</p>
+        <div class="skills-list">
+          <?php while ($row = mysqli_fetch_array($result)) { ?>
+            <div class="flip-container">
+                <div class="flipper">
+                    <div class="front">
+                      <?php echo $row['name']; ?>
                     </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                CSS3
-                            </div>
-                            <div class="back">
-                                This is the style and appearance of your website.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                JavaScript
-                            </div>
-                            <div class="back">
-                                What makes a website interactive on your browser.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                jQuery &amp; AngularJS
-                            </div>
-                            <div class="back">
-                                JavaScript libraries to do cool things with less code.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                PHP
-                            </div>
-                            <div class="back">
-                                A web development language that runs on a server.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                MySQL
-                            </div>
-                            <div class="back">
-                                An open source database system for the web.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                AJAX
-                            </div>
-                            <div class="back">
-                                Loads content with JavaScript asynchronously.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                Server Administration
-                            </div>
-                            <div class="back">
-                                Build, maintain, and update a virtual server.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                CakePHP
-                            </div>
-                            <div class="back">
-                                A PHP framework to rapidly build web applications.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                Drupal
-                            </div>
-                            <div class="back">
-                                A powerful CMS for online content &amp; user profiles.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                WordPress
-                            </div>
-                            <div class="back">
-                                One of the most popular blogging platforms &amp; CMS.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                Joomla
-                            </div>
-                            <div class="back">
-                                A popular &amp; flexible CMS for developing websites.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                Adobe Business Catalyst
-                            </div>
-                            <div class="back">
-                                A business-centered &amp; marketing CMS.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                Project Management
-                            </div>
-                            <div class="back">
-                                The process of planning &amp; organizing a project.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                E-commerce
-                            </div>
-                            <div class="back">
-                                Commercial transactions on the web.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                Responsive Design
-                            </div>
-                            <div class="back">
-                                Making websites optimized for a variety of devices.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                Podcasts
-                            </div>
-                            <div class="back">
-                                Audio/video distributed on the internet to subscribers.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                C#
-                            </div>
-                            <div class="back">
-                                A simple &amp; multi-purpose programming language.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                Virtual Reality Tours
-                            </div>
-                            <div class="back">
-                                A 3D panorama that is manipulated in a browser.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                Audiobooks
-                            </div>
-                            <div class="back">
-                                An audio recording with metadata.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                Mag+ &amp; iBooks
-                            </div>
-                            <div class="back">
-                                Different platforms for digital publication.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                On-page SEO
-                            </div>
-                            <div class="back">
-                                Techniques to improve the search rank of a website.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                Photoshop, Illustrator, InDesign
-                            </div>
-                            <div class="back">
-                                Powerful, industry-standard design software.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                UX Design
-                            </div>
-                            <div class="back">
-                                User Experience Design: Building to enhance user satisfaction and accessibility.
-                            </div>
-                        </div>
+                    <div class="back">
+                      <?php echo $row['description']; ?>
                     </div>
                 </div>
-                <div class="clear-fix"></div>
+            </div>
+          <?php } ?>
+        </div>
+        <div class="clear-fix"></div>
 				<p>Don't see a certain technology for your web project?  Let me know what you need; I'm a quick learner!</p>
 				<form action="contact.php" method="POST">
 					<button type="submit" class="cta-button">Contact Todd</button>
@@ -310,24 +94,24 @@
 			</section>
 		</div>
 	</div>
-	
+
 	<?php
     include('includes/footer.php');
 	?>
-	
+
 	<!-- jQuery and fallback-->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script>
-        if (typeof jQuery == 'undefined') {
-            document.write(unescape("%3Cscript src='/lib/jquery/jquery-1.11.1.min.js' type='text/javascript'%3E%3C/script%3E"));
-        }
-    </script>
-    
-    <!-- Fast Click for mobile -->
-    <script src="lib/fastclick.min.js"></script>
-	
+  <script>
+    if (typeof jQuery == 'undefined') {
+      document.write(unescape("%3Cscript src='/lib/jquery/jquery-1.11.1.min.js' type='text/javascript'%3E%3C/script%3E"));
+    }
+  </script>
+
+  <!-- Fast Click for mobile -->
+  <script src="lib/fastclick.min.js"></script>
+
 	<!-- Custom Scripts -->
 	<script src="js/scripts.js"></script>
-	
+
 </body>
 </html>
