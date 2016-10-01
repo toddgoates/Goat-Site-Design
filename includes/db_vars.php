6406f090efa19c307f181ffe7d3d27cad2934db6
Todd Goates <toddgoates@gmail.com>
2016-10-01 11:20:50 -0600

    Took db_vars.php out of versioning.

--- includes/db_vars.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if(!defined('KEY')) {
-        header('Location: https://toddgoates.com/404');
-    }
-    define('HOST', 'localhost');
-    define('USER', 'toddgoatescom');
-    define('PASS', 'JSdCU33JsDuTQmTp');
-    define('DB', 'toddgoates');
-?>
